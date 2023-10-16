9d61304f773fd0a65e3549e42c195ba2a3dc5ba8
softcafecommon <softcafe.common@gmail.com>
2023-10-16 22:59:27 +0600

    ok

---
+++ server/login/src/main/java/com/ithouse/spring/security/login/controller/PublicController.java
@@ -0,0 +1,10 @@
+package com.ithouse.spring.security.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/public")
+public class PublicController {
+
+}
